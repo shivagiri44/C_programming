860ca907096ab57b5c9ba54a161f0d31b338dadf
shivagiri44 <shivagiri898@gmail.com>
2025-11-20 15:17:58 +0530

    Add files via upload

---
+++  relational operators compare two values.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int a=2;
+int b=7;
+printf("%d > %d is %d \n",a,b,a>b);
+printf("%d < %d is %d \n",a,b,a<b);
+printf("%d >= %d is %d \n",a,b,a>=b);
+printf("%d <= %d is %d \n",a,b,a<=b);
+printf("%d == %d is %d \n",a,b,a==b);
+printf("%d != %d is %d \n",a,b,a!=b);
+}
