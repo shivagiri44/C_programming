c86523cc5fb0b1d94a6667a096cc109783c4ecf5
shivagiri44 <shivagiri898@gmail.com>
2025-11-20 15:19:20 +0530

    Add files via upload

---
+++ even if .c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf("program starts \n");
+printf("enter the number \n");
+scanf("%d",&a);
+if (a%2!=0)
+{
+printf("%d is odd number \n",a);
+}
+else
+{
+printf("%d is even \n",a);
+}
+printf("program ends");
+}
